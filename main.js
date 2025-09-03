58c17af3153ca01dfb08824a34d294805f3aca89
hahaotnmc <hahaotnmc.com@gmail.com>
2025-09-03 16:51:35 +0700

    thêm place holder

--- main.js
+++ main.js
@@ -1,48 +1,13 @@
 //import './lib/styles.css';
 import RichEditor from './index.js';
-
-// Create editor instance with custom toolbar configuration
 const contentContainer = document.getElementById('content-container');
 
 const editor = new RichEditor('#editor-container', {
-  content: 'contentContainer.innerHTML',
-  height: 500,
-  width: 1000,
+  content: "",
   theme: 'light',
-  placeholder: '🎉 Welcome! Try basic formatting: Bold, Italic, Underline, Strikethrough',
-  // Add onChange callback to handle content changes
   onChange: (content) => {
     // Update the output container with new content
     contentContainer.innerHTML = content;
     console.log('Content changed:', content);
-  },
-  // toolbar1: [
-  //   { group: 'text-format', items: ['bold', 'italic', 'underline', 'strike'] },
-  //   { group: 'more', items: ['more'] }
-  // ],
-  // toolbar2: [
-  //   { group: 'structure', items: ['list'] },
-  //   { group: 'table', items: ['table'] }
-  // ]
-});
-const editor2 = new RichEditor('#editor-container2', {
-  content: 'contentContainer.innerHTML',
-  height: 440,
-  width: 1000,
-  theme: 'light',
-  placeholder: '🎉 Welcome! Try basic formatting: Bold, Italic, Underline, Strikethrough',
-  // Add onChange callback to handle content changes
-  onChange: (content) => {
-    // Update the output container with new content
-    contentContainer.innerHTML = content;
-    console.log('Content changed:', content);
-  },
-  // toolbar1: [
-  //   { group: 'text-format', items: ['bold', 'italic', 'underline', 'strike'] },
-  //   { group: 'more', items: ['more'] }
-  // ],
-  // toolbar2: [
-  //   { group: 'structure', items: ['list'] },
-  //   { group: 'table', items: ['table'] }
-  // ]
+  }
 });
